bf67597aded1b1843f148983705ee912da09a832
steveo9219 <stephen.guzman92@gmail.com>
2020-05-24 14:30:44 -0500

    i got the names of the cities to show up but i need the text box to remain there

--- Develop/script.js
+++ Develop/script.js
@@ -5,31 +5,41 @@ var humidHeader = document.getElementById("humidity-header");
 var windHeader = document.getElementById("windspeed-header");
 var uvHeader = document.getElementById("uvindex-header");
 const cityForm = document.forms["city"];
+var cities = document.getElementById("cities");
+let cityValue;
+
+var cityNames = [];
+
+if (localStorage.getItem("cityNames")) {
+  var oldCityNames = localStorage.getItem("cityNames");
+  oldCityNames = oldCityNames.split(",");
+  console.log(oldCityNames);
+  for (var i = 0; i < oldCityNames.length; i++) var city;
+  var oldCityNames = document.createElement("div");
+  cityValue.appendChild = newLink;
+  newLink.innerHTML = oldCityNames[i];
+  //$("#cities").html(`<div> ${oldCityNames} </div>`);
+
+  //cities.create(`${oldCityNames[i]}\n`);
+  console.log(oldCityNames[i]);
+}
 
 cityForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  const cityValue = cityForm.querySelector("input[type=text]").value;
-  console.log(cityValue);
+  cityValue = cityForm.querySelector("input[type=text]").value;
+  cityNames.push(cityValue);
+  localStorage.setItem("cityNames", cityNames);
+  // Here we are building the URL we need to query the database
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=007d67029ca5f1a5a6080f1d55b8a14f`;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    // Log the queryURL
+    // Log the resulting object
+  });
 });
 
-console.log(cityValue);
-
-// Here we are building the URL we need to query the database
-var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?" +
-  cityValue +
-  "&appid=007d67029ca5f1a5a6080f1d55b8a14f";
-
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET",
-})
-  // We store all of the retrieved data inside of an object called "response"
-  .then(function (response) {
-    // Log the queryURL
-    console.log(queryURL);
-
-    // Log the resulting object
-    console.log(response);
-  });
+//main class
