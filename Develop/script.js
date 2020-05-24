b9abd792811c644d65dc078ee88175e885f246a8
steveo9219 <stephen.guzman92@gmail.com>
2020-05-24 16:21:25 -0500

    form populates current searched cityValue while oldCityValue moves down to old city names

--- Develop/script.js
+++ Develop/script.js
@@ -7,9 +7,12 @@ var uvHeader = document.getElementById("uvindex-header");
 var cityForm = document.forms["city"];
 var cities = document.getElementById("cities");
 let cityValue;
-
+//arrays for old cityNames and cityNames
+var oldCityNames = [];
 var cityNames = [];
 console.log(cityNames);
+//
+//
 if (localStorage.getItem("cityNames")) {
   var oldCityNames = localStorage.getItem("cityNames");
   oldCityNames = oldCityNames.split(",");
@@ -18,7 +21,10 @@ if (localStorage.getItem("cityNames")) {
   //PT2. and here i want it to populate from the array cityNames but its just the one city in there, also it should
   //be making DIVs from the forloop below
   for (var i = 0; i < oldCityNames.length; i++) {
-    $("#cities").html(`<div> ${oldCityNames} </div>`);
+    var newLink = document.createElement("div");
+    newLink.innerHTML = oldCityNames[i];
+    cities.appendChild(newLink);
+    //$("#cities").html(`<div> ${oldCityNames} </div>`);
   }
   //cities.create(`${oldCityNames[i]}\n`);
 }
@@ -29,6 +35,7 @@ cityForm.addEventListener("submit", function (e) {
   cityNames.push(cityValue);
   // PT1. after i click the Submit button, it overwrites my previous city value
   localStorage.setItem("cityNames", cityNames);
+  localStorage.setItem("oldCityNames", oldCityNames);
 
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=007d67029ca5f1a5a6080f1d55b8a14f`;
 
